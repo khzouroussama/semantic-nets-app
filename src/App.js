472543230590a4e0308bb15ade9617249587438f
khzouroussama <khzouroussama@gmail.com>
2020-07-02 21:15:30 +0100

    improvement+++

--- src/App.js
+++ src/App.js
@@ -220,7 +220,7 @@ result.length ?
               </Button>
               <div tw="text-sm my-1 text-indigo-400 font-mono p-1 border rounded-full bg-gray-200 shadow-inner mx-5"> <b>{readabledata.nodes.length}</b> Concepts - <b>{readabledata.edges.length}</b> relations</div>
               <div>
-                <Button tw="p-2 py-1"  onClick={()=> cleanGraph()}>
+                <Button tw="p-2 py-1 mt-2"  onClick={()=> cleanGraph()}>
                   <span tw="fill-current w-5 h-5 mr-2">
                     <svg viewBox="0 0 24 24">
                         <path fill="currentColor" d="M2 12C2 9.21 3.64 6.8 6 5.68V3.5C2.5 4.76 0 8.09 0 12S2.5 19.24 6 20.5V18.32C3.64 17.2 2 14.79 2 12M15 3C10.04 3 6 7.04 6 12S10.04 21 15 21 24 16.96 24 12 19.96 3 15 3M20 15.59L18.59 17L15 13.41L11.41 17L10 15.59L13.59 12L10 8.41L11.41 7L15 10.59L18.59 7L20 8.41L16.41 12L20 15.59Z" />
